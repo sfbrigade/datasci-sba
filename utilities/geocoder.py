1e6c332fbd5a36966916a53ed68a1c7302ead0f1
jpspeng <jpspeng@gmail.com>
2017-07-17 22:00:17 -0700

    updated geocoder

--- utilities/geocoder.py
+++ utilities/geocoder.py
@@ -1,6 +1,5 @@
 """
 Geocoding Utility:
-
 We'll be using this: https://github.com/slimkrazy/python-google-places#python-google-places
 to geocode addresses.
 """
@@ -8,39 +7,39 @@ import os
 
 import pandas as pd
 
-from googleplaces import GooglePlaces, types, lang
+from geopy.geocoders import Google V3
 
 YOUR_API_KEY = os.getenv('GOOGLE_PLACES_API')
 
 
 def geocode(df, api_key=YOUR_API_KEY):
     """Add Geocoded columns to df
-
     Keyword Args:
     df: Dataframe which must have an "address" column with a clean address
     api_key: Google Places API Key
     """
-    google_places = GooglePlaces(api_key)
-    matches = []
+    geolocator = GoogleV3(api_key = api_key)
+    latitudes = []
+    longitudes = []
 
     # This counter is just for debugging purposes since I don't want to hit the API threshold
     i = 0
     for place in df.address:
         print(place)
         print(i)
-        query_result = google_places.nearby_search(
-            location=place,
-            radius=100
-        )
-        print(query_result.places)
-        matches.append(query_result.places)
+        query_result = geolocator.geocode(place)
+
+        print(query_result.latitude, query_result.longitude)
+        latitudes.append(query_result.latitude)
+        longitudes.append(query_result.longitude)
         i = i + 1
         if i == 10:
             break
 
-    for i in range(len(matches), len(df)):
-        matches.append(None)
-    df['matches'] = matches
+    for i in range(len(latitudes), len(df)):
+        latitudes.append(None)
+        longitudes.append(None)
+    df['latitudes'] = latitudes
+    df['longitudes'] = longitudes
 
     return df
-
