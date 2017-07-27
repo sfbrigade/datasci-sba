d3d07aec0dfb7f4daffb909c1fde94ae53d78505
VincentLa <vincela14@gmail.com>
2017-07-26 19:25:41 -0700

    Fixing data types

--- pipeline/pipeline_tasks/queries/trg_analytics/00_01_sba_metrics.sql
+++ pipeline/pipeline_tasks/queries/trg_analytics/00_01_sba_metrics.sql
@@ -14,111 +14,82 @@ This query:
 
 drop table if exists trg_analytics.sba_zip_level;
 create table trg_analytics.sba_zip_level
-
 (
-	borr_zip bigint
-	,total_sba int
-	,total_504 int
-	,total_7a int
-	,total_small_bus varchar
-	,mean_agi varchar
-	,sba_per_small_bus float
-	,loan_504_per_small_bus float
-	,loan_7a_per_small_bus float
+  borr_zip text,
+  total_sba int,
+  total_504 int,
+  total_7a int,
+  total_small_bus text,
+  mean_agi text,
+  sba_per_small_bus numeric,
+  loan_504_per_small_bus numeric,
+  loan_7a_per_small_bus numeric
 );
 
 insert into trg_analytics.sba_zip_level
 
 select
-	a_combined.borr_zip
-	,a_combined.total_sba
-	,a_504.total_504
-	,a_7a.total_7a
-	,a_naics.num_establishments as total_small_bus
-	,a_irs.mean_agi as mean_agi
-	,case when a_naics.num_establishments = 0 then null
-          else a_combined.total_sba / a_naics.num_establishments
-     end as sba_per_small_bus
-	,case when a_naics.num_establishments = 0 then null
-	      else a_504.total_504 / a_naics.num_establishments
-	 end as loan_504_per_small_bus
-	,case when a_naics.num_establishments = 0 then null
-	      else a_7a.total_7a / a_naics.num_establishments
-	 end as loan_7a_per_small_bus
+  a_combined.borr_zip,
+  a_combined.total_sba,
+  a_504.total_504,
+  a_7a.total_7a,
+  a_naics.num_establishments as total_small_bus,
+  a_irs.mean_agi as mean_agi,
+  case when a_naics.num_establishments = 0 then null
+        else a_combined.total_sba / a_naics.num_establishments
+  end as sba_per_small_bus,
+  case when a_naics.num_establishments = 0 then null
+       else a_504.total_504 / a_naics.num_establishments
+  end as loan_504_per_small_bus,
+  case when a_naics.num_establishments = 0 then null
+       else a_7a.total_7a / a_naics.num_establishments
+  end as loan_7a_per_small_bus
 
 from
+  (
+  select
+    borr_zip,
+    count(*) as total_sba
+  from stg_analytics.sba_sfdo
+  group by borr_zip
+  ) as a_combined
 
-	(
-	select
-		borr_zip
-		,count(*) as total_sba
-
-	from stg_analytics.sba_sfdo
-
-	group by borr_zip
-
-	) a_combined
-
-	left join
-
-	(
-	select
-		borr_zip
-		,count(*) as total_504
-
-	from stg_analytics.sba_sfdo
-
-	where Program = '504'
-
-	group by borr_zip
-
-	) a_504
-
-	on a_combined.borr_zip = a_504.borr_zip
-
-	left join
-
-	(
-	select
-		borr_zip
-		,count(*) as total_7a
-
-	from stg_analytics.sba_sfdo
-
-	where Program = '7A'
-
-	group by borr_zip
-
-	) a_7a
-
-	on a_combined.borr_zip = a_7a.borr_zip
-
-	left join
-
-	(
-	select
-		num_establishments
-		,geo_id
-		,cast(zipcode as bigint)
-
-	from stg_analytics.census_naics
-
-	where naics2012 = '00'
-
-	) a_naics
-
-	on a_combined.borr_zip = a_naics.zipcode
-
-	left join
-
-	(
-	select
-		cast(zipcode as bigint)
-		,mean_agi
-
-	from stg_analytics.irs_income
-
-	) a_irs
-
-	on a_combined.borr_zip = a_irs.zipcode
+  left join
+  (
+    select
+      borr_zip,
+      count(*) as total_504
+    from stg_analytics.sba_sfdo
+    where program = '504'
+    group by borr_zip
+  ) as a_504
+    on a_combined.borr_zip = a_504.borr_zip
+  left join
+  (
+    select
+      borr_zip,
+      count(*) as total_7a
+    from stg_analytics.sba_sfdo
+    where program = '7A'
+    group by borr_zip
+  ) a_7a
+    on a_combined.borr_zip = a_7a.borr_zip
+  left join
+  (
+    select
+      num_establishments,
+      geo_id,
+      zipcode
+    from stg_analytics.census_naics
+    where naics2012 = '00'
+  ) a_naics
+    on a_combined.borr_zip = a_naics.zipcode
+  left join
+  (
+    select
+      zipcode,
+      mean_agi
+    from stg_analytics.irs_income
+  ) a_irs
+    on a_combined.borr_zip = a_irs.zipcode
 ;
