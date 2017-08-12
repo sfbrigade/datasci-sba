e0cc6ddc088a9574b51fe7930116d61ee69a5a0e
Vincent La <vincela14@gmail.com>
2017-08-12 13:56:54 -0700

    Adding utility file for returning sqlalchemy table definnition and adding another unit test

--- test_suite/unit_tests/test_census_naics.py
+++ test_suite/unit_tests/test_census_naics.py
@@ -4,32 +4,17 @@ import datetime
 import pytest
 import sqlalchemy as sqla
 
+import test_suite.tables as tbls
+
 # This is the Query we are going to test.
 QUERY_FILENAME = '../pipeline/pipeline_tasks/queries/stg_analytics/00_03_census_naics.sql'
 
 TARGET_SCHEMA = 'stg_analytics'
 TARGET_TABLE_NAME = 'census_naics'
 
-METADATA = sqla.MetaData()
-
-DATA_INGEST__CENSUS__ZIP_BUSINESS_PATTERNS = sqla.Table(
-    'census__zip_business_patterns', METADATA,
-    sqla.Column('zipcode', sqla.Text),
-    sqla.Column('geo_id', sqla.Text),
-    sqla.Column('naics2012', sqla.Text),
-    sqla.Column('naics2012_ttl', sqla.Text),
-    sqla.Column('estab', sqla.Text),
-    sqla.Column('empszes_ttl', sqla.Text),
-    schema='data_ingest')
-
-STG_ANALYTICS__SBA_SFDO_ZIPS = sqla.Table(
-    'sba_sfdo_zips', METADATA,
-    sqla.Column('zipcode', sqla.Text),
-    schema='stg_analytics')
-
 SOURCE_TABLES = [
-    DATA_INGEST__CENSUS__ZIP_BUSINESS_PATTERNS,
-    STG_ANALYTICS__SBA_SFDO_ZIPS
+    tbls.DATA_INGEST__CENSUS__ZIP_BUSINESS_PATTERNS,
+    tbls.STG_ANALYTICS__SBA_SFDO_ZIPS
 ]
 
 TARGET_QUERY = 'SELECT * FROM %s.%s' % (TARGET_SCHEMA, TARGET_TABLE_NAME)
