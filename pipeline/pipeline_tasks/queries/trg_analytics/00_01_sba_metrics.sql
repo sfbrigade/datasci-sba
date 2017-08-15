cde42df9dfde128f5ca7dfcb4f8585ea069e88ee
Adam Donovan <avdonovan@gmail.com>
2017-08-15 10:01:06 -0700

    Revert "adding primary key id column to sba_zip_level"
    This reverts commit 1ea1d9017503f41dc0ba1378f5d86daa9210ae95.

--- pipeline/pipeline_tasks/queries/trg_analytics/00_01_sba_metrics.sql
+++ pipeline/pipeline_tasks/queries/trg_analytics/00_01_sba_metrics.sql
@@ -7,7 +7,6 @@ Also computes relevant metrics, sba loans per small businesses.
 drop table if exists trg_analytics.sba_zip_level;
 create table trg_analytics.sba_zip_level
 (
-  id serial primary key,
   borr_zip text,
   mean_agi numeric,
   total_small_bus int,
@@ -16,23 +15,12 @@ create table trg_analytics.sba_zip_level
   total_7a int,
   sba_per_small_bus numeric,
   loan_504_per_small_bus numeric,
-  loan_7a_per_small_bus numeric
+  loan_7a_per_small_bus numeric,
+  primary key(borr_zip)
 );
 
-create index sba_zip_level_borr_zip on trg_analytics.sba_zip_level using btree(borr_zip);
-
 insert into trg_analytics.sba_zip_level
-(
-  borr_zip,
-  mean_agi,
-  total_small_bus,
-  total_sba,
-  total_504,
-  total_7a,
-  sba_per_small_bus,
-  loan_504_per_small_bus,
-  loan_7a_per_small_bus
-)
+
 select
   borr_zip,
   mean_agi,
@@ -63,4 +51,3 @@ from stg_analytics.sba_sfdo as sba
 group by sba.borr_zip, irs.mean_agi, census.num_establishments
 ) as sub
 ;
-
