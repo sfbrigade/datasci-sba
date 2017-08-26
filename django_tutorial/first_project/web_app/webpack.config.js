99ad77f6cce11e8a44812f00cea29ea472a3cbbc
Adam Donovan <avdonovan@gmail.com>
2017-08-26 11:07:36 -0700

    moving all js/css source to static_src directory

--- django_tutorial/first_project/web_app/webpack.config.js
+++ django_tutorial/first_project/web_app/webpack.config.js
@@ -1,10 +1,17 @@
 var path = require("path")
 var webpack = require('webpack')
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = function(env) {
 
   const PROD = env === 'prod'
+
+  const copyWebpackPlugin = new CopyWebpackPlugin([
+      { from: path.join(__dirname, 'static_src') },
+    ], {
+      ignore: ['js/**/*']
+    })
 
   return {
     devtool: PROD ? 'source-map' : 'eval-source-map',
@@ -13,17 +20,19 @@ module.exports = function(env) {
     },
     target: 'web',
     output: {
-      path: path.join(__dirname, 'static/web_app/js'),
-      filename: '[name].bundle.js'
+      path: path.join(__dirname, 'static/web_app'),
+      filename: 'js/[name].bundle.js'
     },
     plugins: PROD ?
     [
       new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')}),
+      copyWebpackPlugin
       // TODO: UglifyJS isn't working, apparently we need to use a different plugin to handle ES6
       // new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
     ] :
     [
       new webpack.NoEmitOnErrorsPlugin(),
+      copyWebpackPlugin
     ],
     module: {
       rules: [
