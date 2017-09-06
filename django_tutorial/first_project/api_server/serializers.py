4b052e898aecc23ad4e3483e0f7e174412065d49
VincentLa <vincela14@gmail.com>
2017-09-05 21:08:55 -0700

    Updating dependencies and adding eserializers

---
+++ django_tutorial/first_project/api_server/serializers.py
@@ -0,0 +1,7 @@
+"""
+Declare serializers that work very similar to Django's forms.
+
+The point of serializers is to provide a functionality to serialize or change
+data into a format that Django uses, such as JSON
+"""
+from rest_framework import serializers
