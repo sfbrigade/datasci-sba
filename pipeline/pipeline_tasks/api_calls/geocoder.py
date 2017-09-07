9c9b7d99af697819a92dc0fccea99d62993919a1
VincentLa <vincela14@gmail.com>
2017-09-06 19:33:41 -0700

    Adding more to geocoder

--- pipeline/pipeline_tasks/api_calls/geocoder.py
+++ pipeline/pipeline_tasks/api_calls/geocoder.py
@@ -2,32 +2,20 @@
 Geocoding Utility:
 """
 import os
-import sys
-sys.path.append('../')
 
 import pandas as pd
 import sqlalchemy as sqla
 from sqlalchemy import create_engine
 
-SBA_DWH = os.getenv('SBA_DWH')
-engine = create_engine(SBA_DWH)
-
 from geopy import GoogleV3
 from utilities.db_manager import DBManager
 
-YOUR_API_KEY = os.getenv('GOOGLE_MAPS_API')
+SBA_DWH = os.getenv('SBA_DWH')
+engine = create_engine(SBA_DWH)
+GOOGLE_API_KEY = os.getenv('GOOGLE_API')
 
 
-def create_address_column(df):
-    """creates a new column on the dataframe for the geocoding address
-    Keyword Args:
-    address: address
-    """
-    df['address'] = df.borr_street + ', ' + df.borr_city + ', ' + df.borr_state + ', ' + df.borr_zip.map(str)
-    return df
-
-
-def geocode_address(address, api_key=YOUR_API_KEY):
+def geocode_address(address, api_key=GOOGLE_API_KEY):
     """Geocodes a single address
 
     Keyword Args:
@@ -43,20 +31,17 @@ def geocode_address(address, api_key=YOUR_API_KEY):
     return latitude, longitude
 
 
-def create_geocode_table(df, dbm):
+def geocode_table(df):
     """Add Geocoded columns to df
 
     Keyword Args:
-    df: Dataframe which must have an "address" column with a clean address
+    df: Dataframe which must have an "full_address" column with a clean address
     api_key: Google Places API Key
     """
-    # create table with standarized address column
-    df = create_address_column(df)
-
     # make list of latitudes and longitudes for each address using list comprehension
     latitudes_longitudes = []
     i = 0
-    for address in df.address:
+    for address in df.full_address:
         print(i)
         latitudes_longitudes.append(geocode_address(address))
         i += 1
@@ -68,21 +53,4 @@ def create_geocode_table(df, dbm):
         longitudes.append(None)
     df['latitudes'] = latitudes
     df['longitudes'] = longitudes
-
-    dbm.write_df_table(df, table_name='table_with_geocode', schema='api_calls')
     return df
-
-
-def main():
-    """Execute Stuff"""
-    print('Geocoding data')
-    with engine.begin() as conn:
-        df = pd.read_sql_table('sba_sfdo', conn, 'stg_analytics')
-    dbm = DBManager(db_url=SBA_DWH)
-    create_geocode_table(df, dbm)
-    print(df)
-
-
-if __name__ == '__main__':
-    """See https://stackoverflow.com/questions/419163/what-does-if-name-main-do"""
-    main()
