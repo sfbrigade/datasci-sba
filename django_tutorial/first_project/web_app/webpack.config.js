8230c892d81bba6b11028d5a667787282fb9168b
Amit Ranan <ranan.amit@gmail.com>
2017-09-13 22:41:45 -0700

    added scss loader

--- django_tutorial/first_project/web_app/webpack.config.js
+++ django_tutorial/first_project/web_app/webpack.config.js
@@ -39,14 +39,16 @@ module.exports = function(env) {
           test: /\.jsx?$/,
           use: ['babel-loader']},
         {
-          test: /\.css$/,
+          test: /\.scss$/,
           use: [{
             loader: "style-loader" // creates style nodes from JS strings
         }, {
-            loader: "css-loader" // translates CSS into CommonJS
-        }, {
             loader: "sass-loader" // compiles Sass to CSS
         }]
+        },
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
         }
       ]
     }
