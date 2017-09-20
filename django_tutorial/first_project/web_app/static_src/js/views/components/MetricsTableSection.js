209410b45a41a81519fc8b9e0ce2994cfed31c64
Adam Donovan <avdonovan@gmail.com>
2017-09-20 08:46:43 -0700

    adding table section

--- django_tutorial/first_project/web_app/static_src/js/views/components/MetricsTableSection.js
+++ django_tutorial/first_project/web_app/static_src/js/views/components/MetricsTableSection.js
@@ -2,26 +2,70 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 
-export default function MetricsTableSection(props) {
-  return (
-    <div>
-      <h2>Rank Header</h2>
-      <table>
-        <tr key={-1}>
-          <td>Rank</td>
-          <td>Business</td>
-          <td>Google Rating</td>
-        </tr>
-        {/* TODO: do a real sortable table, don't just slice */}
-        {Object.values(props.filteredBusinesses).slice(0, 10).map((business, index) => 
-          <tr key={index}>
-            <td>{index+1}</td>
-            <td>{business.borr_name}</td>
-            <td>{business.google_rating}</td>
-          </tr>
-        )}
-      </table>
-    </div>
-  )
+export default class MetricsTableSection extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      numRows: 10,
+      sortBy: 'google_rating'
+    }
+
+    this.comparator = (a, b) => {
+      if(a[this.state.sortBy] < b[this.state.sortBy])
+        return 1
+      else if(a[this.state.sortBy] > b[this.state.sortBy])
+        return -1
+      else
+        return 0
+    }
+
+    this.handleChange = e => this.setState({[e.target.name]: e.target.value})
+  }
+
+  render() {
+    let businesses = Object.values(this.props.filteredBusinesses)
+    businesses.sort(this.comparator)
+    businesses = businesses.slice(0, this.state.numRows)
+    return (
+      <div>
+        <h2>Table</h2>
+        <div>
+          <span>View</span>
+          <select value={this.state.numRows} name="numRows" onChange={this.handleChange}>
+            {[5, 10, 20, 100].map(numRows =>
+              <option value={numRows} key={numRows}>Top {numRows}</option>
+            )}
+          </select>
+          <span>businesses ranked by</span>
+          <select value={this.state.sortBy} name="sortBy" onChange={this.handleChange}>
+            <option value="jobs_supported">Jobs Supported</option>
+            <option value="google_rating">Google Rating</option>
+          </select>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Business</th>
+              <th>Jobs Supported</th>
+              <th>Google Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {businesses.map((business, index) => 
+              <tr key={index}>
+                <td>{index+1}</td>
+                <td>{business.borr_name}</td>
+                <td>{business.jobs_supported}</td>
+                <td>{business.google_rating}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
